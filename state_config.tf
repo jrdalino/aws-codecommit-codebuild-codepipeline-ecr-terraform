7fc46a99f0eabe3a646c48f09ca666ab4b087150
Jose Dee Dalino Jr <Jose.Dalino@excelian.com>
2020-03-17 18:41:27 +0800

    Initial

--- state_config.tf
+++ state_config.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "ap-southeast-2"
     bucket         = "222337787619-ap-southeast-2-terraform-state"
-    key            = "codepipeline.tfstate"
+    key            = "codepipeline-customer.tfstate"
     encrypt        = "true"
     dynamodb_table = "terraform-state-lock"
   }
